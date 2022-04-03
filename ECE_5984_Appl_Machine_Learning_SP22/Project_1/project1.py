f61f43c621e3c700fc1d11256439559927c67527
Andrew Garcia <agarcia1296@yahoo.com>
2022-04-02 18:00:11 -0700

    finished project 1, added research paper on the project

--- ECE_5984_Appl_Machine_Learning_SP22/Project_1/project1.py
+++ ECE_5984_Appl_Machine_Learning_SP22/Project_1/project1.py
@@ -110,6 +110,7 @@ temp_report_df = report_post.statsdf
 for element in tqdm(labels_post):
     if temp_report_df[element][10] > len(df_prep)*0.1: # Weeding out Elements that have more than 10% of missing values
         df_final = df_final.drop(columns = [element])
+        print('ELEMENT Dropped:', element)
     elif temp_report_df[element][10] < len(df_prep)*0.1:
         if element == 'NEXTDAYPRECIPAMT' or element == 'NEXTDAYPRECIPFLAG':
             avg_value = 0
@@ -117,8 +118,7 @@ for element in tqdm(labels_post):
             avg_value = temp_report_df[element][1]
         replace_missing_values_avg(df_final, element, avg_value)
 
-#%%
-# Run Quality Report and Output Data to Excel
+#%% Run Quality Report and Output Data to Excel
 df_final.to_excel('Weather_Data_Final.xlsx')
 
 labels_final = df_final.columns
@@ -130,56 +130,248 @@ for thisLabel in tqdm(labels_final): # for each column, report stats
     report_final.addCol(thisLabel, thisCol)
 
 #print(report.to_string())
-report_final.statsdf.to_excel("Quality_Report_Final.xlsx")
+#report_final.statsdf.to_excel("Quality_Report_Final.xlsx")
 
 #%% Setting up Training Data
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-import numpy as np
-
 # Data
-X_labels = df_final.columns.drop(['NEXTDAYPRECIPFLAG','NEXTDAYPRECIPAMT'])
-X = df_final[X_labels]
+feature_names = df_final.columns.drop(['NEXTDAYPRECIPFLAG','NEXTDAYPRECIPAMT'])
+X = df_final[feature_names]
 
 # Target
 y_precip_flag = df_final.loc[:, ['NEXTDAYPRECIPFLAG']]
-y_precip_amt = df_final.loc[:, ['NEXTDAYPRECIPAMT']]
+labels = y_precip_flag['NEXTDAYPRECIPFLAG'].unique()
 
-#%% Decision Tree - Next Day Precip Flag
-from utils_project1 import writegraphtofile
+#%% Create Testing and Training data for Precip Flag
+from sklearn.model_selection import train_test_split
+import numpy as np
+from utils_project1 import writegraphtofile, get_true_positive
 from sklearn import tree
 
-# Create Testing and Training data
-X_train, X_test, y_train, y_test = train_test_split(X, y_precip_flag, test_size=0.3, train_size=0.7, random_state=1, shuffle=True, stratify=None)
-
-# Create Decision Tree
-clf_entropy = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = 4)
-clf_entropy = clf_entropy.fit(X_train, np.array(y_train['NEXTDAYPRECIPFLAG']))
-print("Training set score = ", clf_entropy.score(X_train, y_train['NEXTDAYPRECIPFLAG']))
-print("Test set score = ", clf_entropy.score(X_test, y_test['NEXTDAYPRECIPFLAG']))
+X_train_flag, X_test_flag, y_train_flag, y_test_flag = train_test_split(X, y_precip_flag, test_size=0.3, 
+                                                                        train_size=0.7, random_state=1996, 
+                                                                        shuffle=True, stratify=None)
+#%% Create Decision Tree - Entropy
+score_df = pd.DataFrame()
+idx = 0
+for i in range(3,9):
+    clf_entropy = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = i)
+    clf_entropy = clf_entropy.fit(X_train_flag, np.array(y_train_flag['NEXTDAYPRECIPFLAG']))
+    
+    training_score = clf_entropy.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG'])
+    true_positive_entropy, matrix_df_entropy = get_true_positive(clf_entropy, X_test_flag, y_test_flag)
+    
+    score_df.loc[idx,'Tree Depth'] = i
+    score_df.loc[idx,'Entropy Training Score'] = training_score
+    score_df.loc[idx,'Entropy Test Score'] = true_positive_entropy
+    
+    idx = idx+1
+    
+# Measure Performance
+print("Entropy Training set score = ", clf_entropy.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG']))
+print("Entropy Test set score = ", clf_entropy.score(X_test_flag, y_test_flag['NEXTDAYPRECIPFLAG']))
+print('Entropy True Positive Rate = ', true_positive_entropy)
 
 # Create Graphic
-target_unique = y_precip_flag['NEXTDAYPRECIPFLAG'].unique()
-path_name = os.path.join(dir_name, "Weather_Data_DecisionTree_Entropy.png")
-writegraphtofile(clf_entropy, X_labels, (str(target_unique[0]), str(target_unique[1])), path_name)
+path_name = os.path.join(dir_name, "Weather_Data_DecisionTree_Entropy_NextDayPrecipFlag.png")
+writegraphtofile(clf_entropy, feature_names, (str(labels[0]), str(labels[1])), path_name)
 tree.export_graphviz(clf_entropy)
 
-#%% 
+#%% Create Decision Tree - Gini
+idx = 0
+for i in range(3,9):
+    clf_gini = tree.DecisionTreeClassifier(criterion = "gini", max_depth = i)
+    clf_gini = clf_gini.fit(X_train_flag, np.array(y_train_flag['NEXTDAYPRECIPFLAG']))
+    
+    training_score = clf_gini.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG'])
+    true_positive_gini, matrix_df_gini = get_true_positive(clf_gini, X_test_flag, y_test_flag)
+    
+    score_df.loc[idx,'Gini Training Score'] = training_score
+    score_df.loc[idx,'Gini Test Score'] = true_positive_gini
+    
+    idx = idx+1
+    
+# Measure Performance
+print("Gini Training set score = ", clf_gini.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG']))
+print("Gini Test set score = ", clf_gini.score(X_test_flag, y_test_flag['NEXTDAYPRECIPFLAG']))
+print('Gini True Positive Rate = ', true_positive_gini)
 
 
-# Next Day Precip Amt
-X_train, X_test, y_train, y_test = train_test_split(X, y_precip_amt, test_size=0.3, train_size=0.7,random_state=1, shuffle=True, stratify=None)
+# Create Graphic
+path_name = os.path.join(dir_name, "Weather_Data_DecisionTree_Gini_NextDayPrecipFlag.png")
+writegraphtofile(clf_gini, feature_names, (str(labels[0]), str(labels[1])), path_name)
+tree.export_graphviz(clf_gini)
+
+#%% Linear Regression
+# Target
+y_precip_amt = df_final.loc[:, ['NEXTDAYPRECIPAMT']]
+labels_amt = y_precip_amt['NEXTDAYPRECIPAMT'].unique()
+
+# Split training/testing data by precip amt
+X_train_amt, X_test_amt, y_train_amt, y_test_amt = train_test_split(X, y_precip_amt, test_size=0.3, 
+                                                                    train_size=0.7, random_state=1996, 
+                                                                    shuffle=True, stratify=None)
+
+from sklearn.linear_model import LinearRegression
+from utils_project1 import get_mse
+from sklearn.metrics import mean_squared_error
+
+linreg_model = LinearRegression().fit(X_train_amt, np.array(y_train_amt['NEXTDAYPRECIPAMT']))
+linreg_model.score(X_test_amt, y_test_amt)
+
+# Testing score
+lin_model_pred_test = linreg_model.predict(X_test_amt)
+mean_squared_error(y_test_amt, lin_model_pred_test)
+
+print('Coefficients:', linreg_model.coef_)
+print('Intercept:', linreg_model.intercept_)
+print('Mean squared error (MSE): %.2f'
+      % mean_squared_error(y_test_amt, lin_model_pred_test))
+
+#%% RidgeCV
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+from sklearn.linear_model import RidgeCV
+
+ridge_model = RidgeCV().fit(X_train_amt, np.array(y_train_amt['NEXTDAYPRECIPAMT']))
+
+# Testing score
+ridge_model_pred_test = ridge_model.predict(X_test_amt)
+mean_squared_error(y_test_amt, ridge_model_pred_test)
+
+print('Coefficients:', ridge_model.coef_)
+print('Intercept:', ridge_model.intercept_)
+print('Mean squared error (MSE): %.2f'
+      % mean_squared_error(y_test_amt, ridge_model_pred_test))
+#%% Measure Mean Square Error
+
+print("Mean Square Error = ", get_mse(linreg_model, X_test_amt, y_test_amt))
+print("Mean Square Error = ", get_mse(ridge_model, X_test_amt, y_test_amt))
+
+#%% Use of Prior Days Data - THIS TAKES SEVERAL MINUTES
+from utils_project1 import create_prior_day_data_df
+feature_names = df_final.columns.drop(['NEXTDAYPRECIPFLAG','NEXTDAYPRECIPAMT'])
+df_prior_day = df_final.copy()
+#Create the DF for prior day
+create_prior_day_data_df(df_prior_day, feature_names)
+
+#%% Setting up Training Data
+# Data
+feature_names = df_prior_day.columns.drop(['NEXTDAYPRECIPFLAG','NEXTDAYPRECIPAMT'])
+X = df_prior_day[feature_names]
+
+# Target
+y_precip_flag = df_prior_day.loc[:, ['NEXTDAYPRECIPFLAG']]
+labels = y_precip_flag['NEXTDAYPRECIPFLAG'].unique()
+
+#%% Create Testing and Training data for Precip Flag
+
+X_train_flag, X_test_flag, y_train_flag, y_test_flag = train_test_split(X, y_precip_flag, test_size=0.3, 
+                                                                        train_size=0.7, random_state=1996, 
+                                                                        shuffle=True, stratify=None)
+
+
+#%% Create Decision Tree - Entropy
+idx = 0
+for i in range(3,9):
+    clf_entropy = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = i)
+    clf_entropy = clf_entropy.fit(X_train_flag, np.array(y_train_flag['NEXTDAYPRECIPFLAG']))
+    
+    training_score = clf_entropy.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG'])
+    true_positive_entropy, matrix_df_entropy = get_true_positive(clf_entropy, X_test_flag, y_test_flag)
+
+    score_df.loc[idx,'Entropy Training Score Prior Day Data'] = training_score
+    score_df.loc[idx,'Entropy Test Score Prior Day Data'] = true_positive_entropy
+    
+    idx = idx+1
+
+# Measure Performance
+print("Entropy Training set score = ", clf_entropy.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG']))
+print("Entropy Test set score = ", clf_entropy.score(X_test_flag, y_test_flag['NEXTDAYPRECIPFLAG']))
+print('Entropy True Positive Rate = ', true_positive_entropy)
+
+# Create Graphic
+path_name = os.path.join(dir_name, "Weather_Data_DecisionTree_Entropy_NextDayPrecipFlag_PriorDay.png")
+writegraphtofile(clf_entropy, feature_names, (str(labels[0]), str(labels[1])), path_name)
+tree.export_graphviz(clf_entropy)
+
+#%% Create Decision Tree - Gini
+idx = 0 
+for i in range(3,9):
+    clf_gini = tree.DecisionTreeClassifier(criterion = "gini", max_depth = i)
+    clf_gini = clf_gini.fit(X_train_flag, np.array(y_train_flag['NEXTDAYPRECIPFLAG']))
+    
+    training_score = clf_gini.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG'])
+    true_positive_gini, matrix_df_gini = get_true_positive(clf_gini, X_test_flag, y_test_flag)
+
+    score_df.loc[idx,'Gini Training Score Prior Day Data'] = training_score
+    score_df.loc[idx,'Gini Test Score Prior Day Data'] = true_positive_gini
+    
+    idx = idx+1
+
+# Measure Performance
+print("Gini Training set score = ", clf_gini.score(X_train_flag, y_train_flag['NEXTDAYPRECIPFLAG']))
+print("Gini Test set score = ", clf_gini.score(X_test_flag, y_test_flag['NEXTDAYPRECIPFLAG']))
+print('Gini True Positive Rate = ', true_positive_gini)
+
+# Create Graphic
+path_name = os.path.join(dir_name, "Weather_Data_DecisionTree_Gini_NextDayPrecipFlag_PriorDay.png")
+writegraphtofile(clf_gini, feature_names, (str(labels[0]), str(labels[1])), path_name)
+tree.export_graphviz(clf_gini)
+
+#%% Exporting Score DF
+score_df.to_excel('Decision Tree Scores.xlsx')
+
+#%% Linear Regression - Using Prior Day Data
+# Target
+y_precip_amt = df_final.loc[:, ['NEXTDAYPRECIPAMT']]
+labels_amt = y_precip_amt['NEXTDAYPRECIPAMT'].unique()
+
+# Split training/testing data by precip amt
+X_train_amt, X_test_amt, y_train_amt, y_test_amt = train_test_split(X, y_precip_amt, test_size=0.3, 
+                                                                    train_size=0.7, random_state=1996, 
+                                                                    shuffle=True, stratify=None)
+
+from sklearn.linear_model import LinearRegression
+from utils_project1 import get_mse
+
+linreg_model = LinearRegression().fit(X_train_amt, np.array(y_train_amt['NEXTDAYPRECIPAMT']))
+linreg_model.score(X_test_amt, y_test_amt)
+
+# Testing score
+lin_model_pred_test = linreg_model.predict(X_test_amt)
+mean_squared_error(y_test_amt, lin_model_pred_test)
+
+print('Coefficients:', linreg_model.coef_)
+print('Intercept:', linreg_model.intercept_)
+print('Mean squared error (MSE): %.2f'
+      % mean_squared_error(y_test_amt, lin_model_pred_test))
 
 
 
 
+#%% RidgeCV
+from sklearn.model_selection import cross_val_score, RepeatedKFold
+from sklearn.linear_model import RidgeCV
 
+ridge_model = RidgeCV().fit(X_train_amt, np.array(y_train_amt['NEXTDAYPRECIPAMT']))
 
+# Testing score
+ridge_model_pred_test = ridge_model.predict(X_test_amt)
+mean_squared_error(y_test_amt, ridge_model_pred_test)
 
+print('Coefficients:', ridge_model.coef_)
+print('Intercept:', ridge_model.intercept_)
+print('Mean squared error (MSE): %.2f'
+      % mean_squared_error(y_test_amt, ridge_model_pred_test))
 
+#%% Measure Mean Square Error
 
+print("Mean Square Error = ", get_mse(linreg_model, X_test_amt, y_test_amt))
+print("Mean Square Error = ", get_mse(ridge_model, X_test_amt, y_test_amt))
 
-
+#%% Make Plot
+import matplotlib.pyplot as plt
+plt.scatter(X_test_amt, y_test_amt, color = 'black')
+plt.plot(X_test_amt, lin_model_pred_test, color="blue", linewidth=3)
 
 
 
--- ECE_5984_Appl_Machine_Learning_SP22/Project_1/utils_project1.py
+++ ECE_5984_Appl_Machine_Learning_SP22/Project_1/utils_project1.py
@@ -9,6 +9,46 @@ import pandas as pd
 from sklearn import tree
 import pydotplus 
 import collections 
+from tqdm import tqdm
+from sklearn import metrics
+
+def create_prior_day_data_df(df, feature_names):
+    for feature in feature_names:
+        for idx in tqdm(df.index):
+            if idx == 0:
+                df.loc[idx, 'PREV_'+feature] = 0
+            elif idx > 0:
+                df.loc[idx, 'PREV_'+feature] = df.loc[idx-1, feature] 
+    return df
+
+def get_mse(model, x, y):
+    model_pred_test = model.predict(x)
+    sum_ = 0
+    idx_pred = 0
+    for idx_y in y.index:
+        a = (y.loc[idx_y, 'NEXTDAYPRECIPAMT'] - model_pred_test[idx_pred])**2
+        sum_ = sum_ + a
+        idx_pred = idx_pred+1
+    mse = (1/len(model_pred_test)*sum_)
+    return mse
+
+def get_true_positive(decision_tree, x_test, y_test):
+    """
+    Returns
+    -------
+    true_positive_value : float64
+        Results from the total correctly predicited divided by total predictions.
+    matrix_df : DataFrame
+        Predicted Labels are Columns and True Labels are rows. 
+
+    """
+    test_pred_decision_tree = decision_tree.predict(x_test)
+    confusion_matrix = metrics.confusion_matrix(y_test, test_pred_decision_tree)
+    #turn this into a dataframe
+    matrix_df = pd.DataFrame(confusion_matrix)
+    test_set_correctly_pred = matrix_df[0][0] + matrix_df[1][1]
+    true_positive = test_set_correctly_pred / len(x_test)
+    return true_positive, matrix_df
 
 def replace_missing_values_avg(df, column_name, avg_value):
     """
@@ -46,6 +86,34 @@ def writegraphtofile(clf, feature_labels, classnames, pathname):
 class Weather_Data_CSV:
     def __init__(self, csv_path):
         self.data_raw = pd.read_csv(csv_path)
+        self.df_prep
+        
+    def get_unique_column_values(df):
+        """
+        Identifying Unique Values of each Column in DF
+        Output is a Dictionary of each Column
+        """
+        headers_unique = {}
+        for label in tqdm(df.columns):
+            headers_unique[label] = df[label].unique()
+        #pbar.close()
+        return headers_unique
+
+    def prep_data(df, df_out, headers_unique):    
+        """
+        Extract Values for Elements and insert into df_prep
+        """
+        index_ = 0 
+        for date in tqdm(headers_unique['DATE']):
+            date_idx = df['DATE'] == date
+            df_by_date = df[date_idx]
+            df_out.loc[index_, 'DATE'] = date 
+            for idx in df_by_date['ELEMENT'].index:
+                df_out.loc[index_, df_by_date['ELEMENT'][idx]] = df_by_date['VALUE1'][idx]
+            index_ = index_+1
+            
+    def addCol(self, label):
+        pass
 
 
 class StatsReport:
@@ -55,7 +123,12 @@ class StatsReport:
         pass
     
     def addCol(self, label, data):
-        self.statsdf[label] = [self.cardinality_(data), self.mean_(data), self.median_(data), self.n_at_median(data), self.mode_(data), self.n_at_mode(data), self.std_(data), self.min_(data), self.max_(data), self.nzero_(data), self.nmissing_(data)]
+        self.statsdf[label] = [self.cardinality_(data), self.mean_(data), 
+                               self.median_(data), self.n_at_median(data), 
+                               self.mode_(data), self.n_at_mode(data), 
+                               self.std_(data), self.min_(data), 
+                               self.max_(data), self.nzero_(data), 
+                               self.nmissing_(data)]
         
     def to_string(self):
         return self.statsdf.to_string()
